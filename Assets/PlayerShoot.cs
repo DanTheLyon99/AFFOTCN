c796d393ee1c83d60faebca12ec815ada817f4bb
Daniel Lyon <dlyon@uoguelph.ca>
2020-06-17 13:06:13 -0400

    Added Crosshair Commit  3
    Added Crosshair

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sets cursor to crosshair texture
+        Cursor.SetCursor(cursorTexture,Vector2.zero, CursorMode.Auto);
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
